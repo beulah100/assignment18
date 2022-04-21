6231b57dd3a7b4208fee1425a2eaa8645bf94107
beulah100 <chinemerefrancisekene@hotmail.com>
2022-04-21 14:10:39 +0100

    assignment 18

---
+++ .js
@@ -0,0 +1,9 @@
+function lifeInWeeks(){
+    var age = (lifeInWeeks());
+    var life = parseInt(90 -"age");
+    var Months = parseInt(12 * "life");
+    var weeks = parseInt(4 * "Months");
+    var days = parseInt(7 * "Weeks");
+    var output = " You have" + Days + "Days" + Weeks +" Weeks"+ Months + " Months"
+    console.log(output)
+}
